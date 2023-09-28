97b8311e3cc76379dc2d0d36a32b2cad3ffb29e3
boomshakazulu <dallonlahoda@hotmail.com>
2023-09-28 05:36:44 -0600

    filtered weather api results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-var api = "2504341f8dff556dc719a3a0672777f0"
+var APIKey = "2504341f8dff556dc719a3a0672777f0"
+var textArea = document.getElementById("textArea")
+var searchBtn = document.getElementById("searchBtn")
+var city = textArea.value
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+searchBtn.addEventListener("click", function() {
+    var city = textArea.value.split(/\s+/).join("+")
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    fetch(queryURL).then(response => response.json()).then(data => { 
+       var lat = data.coord.lat
+       var lon = data.coord.lon
+       console.log(data)
+        getWeatherData(lat, lon) 
+    })
+});
+
+function getWeatherData(lat, lon) {
+    var APIReq= "https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat="+lat+"&lon="+lon+"&appid="+APIKey
+    fetch(APIReq).then(res => res.json()).then(data => {
+        var uniqueForcastDays = [];
+        var fiveDay = data.list.filter(forcast => {
+            var forcastDate= new Date(forcast.dt_txt).getDate()
+            if(!uniqueForcastDays.includes(forcastDate)) {
+                return uniqueForcastDays.push(forcastDate)
+            }
+                 
+    })
+    console.log(fiveDay)  
+    })
+}
